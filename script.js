2732effc364710e94ff69ef9109fd80679d919ec
GabVasconcelos <gabrielvasconcelos2709@gmail.com>
2022-04-03 15:41:51 -0300

    Add color transitioning

--- script.js
+++ script.js
@@ -8,17 +8,21 @@ function clamp(min, max, num) {
 function drawcanvas(size) {
 	container.innerHTML = ""
 	container.style["grid-template-columns"] = `repeat(${size}, 10px)`
+	container.style["grid-template-rows"] = `repeat(${size}, 10px)`
 	for (let x=1; x<=size; x++) {
 		for (let y=1; y<=size; y++){
 			let div = document.createElement("div")
+			let color = 255
 			div.classList.add("pixel")
 			container.appendChild(div)
 			div.addEventListener("mouseover", (event) => {
 				event.target.classList.add("hover")
+				color -= 25.5
+				div.style.backgroundColor = `rgba(0, ${color}, ${color}, 1)`
 			})
-			div.addEventListener("mouseout", (event) => {
-				event.target.classList.remove("hover")
-			})
+			// div.addEventListener("mouseout", (event) => {
+			// 	event.target.classList.remove("hover")
+			// })
 		}
 	}
 }
