21a17e82d89a0b3ef508697de57e411259225bcd
GabVasconcelos <gabrielvasconcelos2709@gmail.com>
2022-04-03 15:27:14 -0300

    Add main logic

---
+++ script.js
@@ -0,0 +1,32 @@
+const container = document.querySelector(".container")
+const clearbutton = document.querySelector(".clear")
+
+function clamp(min, max, num) {
+	return Math.min(Math.max(num, min), max)
+}
+
+function drawcanvas(size) {
+	container.innerHTML = ""
+	container.style["grid-template-columns"] = `repeat(${size}, 10px)`
+	for (let x=1; x<=size; x++) {
+		for (let y=1; y<=size; y++){
+			let div = document.createElement("div")
+			div.classList.add("pixel")
+			container.appendChild(div)
+			div.addEventListener("mouseover", (event) => {
+				event.target.classList.add("hover")
+			})
+			div.addEventListener("mouseout", (event) => {
+				event.target.classList.remove("hover")
+			})
+		}
+	}
+}
+
+clearbutton.addEventListener("click", event => {
+	let size = prompt("What size do you want now?")
+	size = Number(size) && clamp(16, 100, size) || 32
+	drawcanvas(size)
+})
+
+drawcanvas(32)
